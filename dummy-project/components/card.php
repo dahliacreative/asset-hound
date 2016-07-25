8f1feaae8694f8cfbb9a6f743a4dbc61b45679b5
dahliacreative <simon@dahliacreative.com>
2016-07-25 09:35:11 +0100

    Updating logo to svg

--- dummy-project/components/card.php
+++ dummy-project/components/card.php
@@ -23,36 +23,36 @@ $modifier_classes = (is_array($modifiers) && count($modifiers) >=1 ? " card--" .
 <?php $modifier_classes = (is_array($modifiers) && count($modifiers) >=1 ? ' card--' . implode(' card--', $modifiers) : ""); ?>
 <article class="card <?php echo $modifier_classes; ?>">
     <?php if($href) : ?>
-        <a href="<?php echo $href; ?>">
+    <a href="<?php echo $href; ?>">
     <?php endif; ?>
     <?php if($modifiers && in_array('document', $modifiers) && !in_array('no-image', $modifiers)): ?>
-        <div class="card__media-wrap">
-            <?php if($image_src): ?>
-                <img src="<?php echo $image_src ?>" alt="<?php echo $image_alt; ?>" class="card__media"/>
-            <?php else: ?>
-                <img src="<?php echo $view->getThemePath()?>/app/images/content/brochure.jpg" alt="<?php echo $image_alt; ?>" class="card__media"/>
-            <?php endif; ?>
-        </div>
+    <div class="card__media-wrap">
+    <?php if($image_src): ?>
+        <img src="<?php echo $image_src ?>" alt="<?php echo $image_alt; ?>" class="card__media"/>
+    <?php else: ?>
+         <img src="<?php echo $view->getThemePath()?>/app/images/content/brochure.jpg" alt="<?php echo $image_alt; ?>" class="card__media"/>
+    <?php endif; ?>
+    </div>
     <?php elseif($image_src): ?>
-        <div class="card__media-wrap">
-            <img src="<?php echo $image_src; ?>" alt="<?php echo $image_alt; ?>" class="card__media"/>
-        </div>
+    <div class="card__media-wrap">
+        <img src="<?php echo $image_src; ?>" alt="<?php echo $image_alt; ?>" class="card__media"/>
+    </div>
     <?php endif; ?>
     <div class="card__content">
         <h1 class="card__title"><?php echo $title; ?></h1>
         <?php if($subtitle) : ?>
-            <p class="card__subtitle"><?php echo $subtitle; ?></p>
+        <p class="card__subtitle"><?php echo $subtitle; ?></p>
         <?php endif; ?>
         <p class="card__copy"><?php echo $content; ?></p>
         <?php if($date) : ?>
-            <time class="date"><?php echo $date; ?></time>
+        <time class="date"><?php echo $date; ?></time>
         <?php endif; ?>
         <?php if($component) : ?>
-            <?php $this->inc('components/'.$component.'.php', $component_data); ?>
+        <?php $this->inc('components/'.$component.'.php', $component_data); ?>
         <?php endif; ?>
     </div>
     <?php if($href) : ?>
-        </a>
+    </a>
     <?php endif; ?>
 </article>
 <!-- /component -->
--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <header class="ah-header">
   <div class="ah-container">
-    <img src="images/logo.png" alt="Asset Hound" class="ah-header__logo">
+    <img src="images/logo.svg" alt="Asset Hound" class="ah-header__logo">
     <div class="ah-header__control">
       <p class="ah-header__project"><?php echo $projectName; ?></p>
       <button type="button" class="ah-menu"><span></span><span></span></button>
